fb0a10901dd29f21e385b885ea23e024d186bfd8
swaroop <S525756@mail.nwmissouri.edu>
2026-01-03 22:49:21 -0500

    Initialize App component with routing setup

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <HeaderComponent />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<ListEmployeeComponent />} />
+            <Route path="/employees" element={<ListEmployeeComponent />} />
+            <Route path="/add-employee" element={<CreateEmployeeComponent />} />
+            <Route path="/update-employee/:id" element={<CreateEmployeeComponent />} />
+            <Route path="/view-employee/:id" element={<ViewEmployeeComponent />} />
+          </Routes>
+        </div>
+        <FooterComponent />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
