05ad537c73e3946b9b168045c7bc5d607073e462
swaroop <S525756@mail.nwmissouri.edu>
2026-01-03 22:43:55 -0500

    Initialize Spring Boot application with CORS support

---
+++ backend/src/main/java/com/fullstack/crud/FullstackCrudApplication.java
@@ -0,0 +1,14 @@
+package com.fullstack.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@SpringBootApplication
+@CrossOrigin(origins = "http://localhost:3000")
+public class FullstackCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackCrudApplication.class, args);
+    }
+}
